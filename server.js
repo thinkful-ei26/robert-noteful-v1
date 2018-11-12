7cd6e3f1a512c2846c3d1b7d4654954e70bcc739
RP Boyle <rpboyle11@yahoo.com>
2018-11-12 14:33:24 -0500

    the first commit

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 'use strict';
 
+const express = require('express');
 // Load array of notes
+
 const data = require('./db/notes');
 
+const app = express();
+
 console.log('Hello Noteful!');
+
+app.listen(8080, function () {
+  console.info(`Server listening on ${this.address().port}`);  
+}).on('error', err => {
+  console.error(err);
+});
 
 // INSERT EXPRESS APP CODE HERE...
