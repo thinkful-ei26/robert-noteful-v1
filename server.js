877719aa1d6824f215ba694f2fa59766b182af00
RP Boyle <rpboyle11@yahoo.com>
2018-11-13 16:53:18 -0500

    updated the GET /api/notes function

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require('express');
 // Load array of notes
 
 const data = require('./db/notes');
+const simDB = require('./db/simDB');
+const notes = simDB.initialize(data);
 
 const { PORT } = require('./config');
 
@@ -15,6 +17,17 @@ app.use(express.static('public'));
 
 app.use(logger);
 
+app.get('/api/notes', (req, res, next) => {
+  const { searchTerm } = req.query;
+
+  notes.filter(searchTerm, (err, list) => {
+    if (err) {
+      return next(err); // goes to error handler
+    }
+    res.json(list); // responds with filtered array
+  });
+});
+
 app.get('/api/notes/:id', (req, res) => {
   const id = req.params.id;
   let note = data.find(function(item) {
@@ -23,17 +36,18 @@ app.get('/api/notes/:id', (req, res) => {
   res.json(note);
 });
 
-app.get('/api/notes', (req, res) => {
-  //lets us retrieve the searchTerm from the query.string on req.query object
-  const searchTerm = req.query.searchTerm;
-  //filter through the array from searchterm and return results
-  if (searchTerm) {
-    let filtered = data.filter(item => item.title.includes(searchTerm));
-    res.json(filtered);
-  } else {
-    res.json(data);
-  }
-});
+// app.get('/api/notes', (req, res) => {
+//   //lets us retrieve the searchTerm from the query.string on req.query object
+//   const searchTerm = req.query.searchTerm;
+//   //filter through the array from searchterm and return results
+//   if (searchTerm) {
+//     let filtered = data.filter(item => item.title.includes(searchTerm));
+//     res.json(filtered);
+//   } else {
+//     res.json(data);
+//   }
+// });
+
 
 app.get('/boom', (req, res, next) => {
   throw new Error('Boom!!');
