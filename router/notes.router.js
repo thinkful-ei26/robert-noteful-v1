003362d2fa67e7faa40d521e714ff27159a28773
RP Boyle <rpboyle11@yahoo.com>
2018-11-14 14:54:20 -0500

    moved endpoints to express router

---
+++ router/notes.router.js
@@ -0,0 +1,71 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+const data = require('../db/notes');
+const simDB = require('../db/simDB');
+const notes = simDB.initialize(data);
+
+router.get('/notes', (req, res, next) => {
+  const { searchTerm } = req.query;
+
+  notes.filter(searchTerm, (err, list) => {
+    if (err) {
+      return next(err); // goes to error handler
+    }
+    res.json(list); // responds with filtered array
+  });
+});
+
+router.get('/notes/:id', (req, res) => {
+  const id = req.params.id;
+
+  notes.find(id, (err, item) => {
+    if (err) {
+      return next(err);
+    }
+    if (item) {
+      res.json(item);
+    } else {
+      next();
+    }
+  });
+});
+
+router.put('/notes/:id', (req, res, next) => {
+  const id = req.params.id;
+
+  const updateObj = {};
+  const updateableFields = ['title', 'content'];
+
+  updateableFields.forEach(field => {
+    if (field in req.body) {
+      updateObj[field] = req.body[field];
+    }
+  });
+
+  if (!updateObj.title) {
+    const err = new Error('Missing `title` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  notes.update(id, updateObj, (err, item) => {
+    if (err) {
+      return next(err);
+    }
+    if (item) {
+      res.json(item);
+    } else {
+      next();
+    }
+  });
+});
+
+router.get('/boom', (req, res, next) => {
+  throw new Error('Boom!!');
+});
+  
+
+module.exports = router;
--- server.js
+++ server.js
@@ -4,15 +4,15 @@ const express = require('express');
 const morgan = require('morgan');
 // Load array of notes
 
-const data = require('./db/notes');
-const simDB = require('./db/simDB');
-const notes = simDB.initialize(data);
-
 const { PORT } = require('./config');
+const notesRouter = require('./router/notes.router.js');
+
+// const data = require('./db/notes');
+// const simDB = require('./db/simDB');
+// const notes = simDB.initialize(data);
+
 
 const app = express();
-
-// const logger = require('./middleware/logger.js');
 
 app.use(express.static('public'));
 
@@ -20,80 +20,9 @@ app.use(express.json());
 
 app.use(morgan('dev'));
 
-// app.use(logger);
-
-app.get('/api/notes', (req, res, next) => {
-  const { searchTerm } = req.query;
-
-  notes.filter(searchTerm, (err, list) => {
-    if (err) {
-      return next(err); // goes to error handler
-    }
-    res.json(list); // responds with filtered array
-  });
-});
-
-app.get('/api/notes/:id', (req, res) => {
-  const id = req.params.id;
-
-  notes.find(id, (err, item) => {
-    if (err) {
-      return next(err);
-    }
-    if (item) {
-      res.json(item);
-    } else {
-      next();
-    }
-  });
-});
-
-app.put('/api/notes/:id', (req, res, next) => {
-  const id = req.params.id;
-
-  const updateObj = {};
-  const updateableFields = ['title', 'content'];
-
-  updateableFields.forEach(field => {
-    if (field in req.body) {
-      updateObj[field] = req.body[field];
-    }
-  });
-
-  if (!updateObj.title) {
-    const err = new Error('Missing `title` in request body');
-    err.status = 400;
-    return next(err);
-  }
-
-  notes.update(id, updateObj, (err, item) => {
-    if (err) {
-      return next(err);
-    }
-    if (item) {
-      res.json(item);
-    } else {
-      next();
-    }
-  });
-});
-
-// app.get('/api/notes', (req, res) => {
-//   //lets us retrieve the searchTerm from the query.string on req.query object
-//   const searchTerm = req.query.searchTerm;
-//   //filter through the array from searchterm and return results
-//   if (searchTerm) {
-//     let filtered = data.filter(item => item.title.includes(searchTerm));
-//     res.json(filtered);
-//   } else {
-//     res.json(data);
-//   }
-// });
+app.use('/api', notesRouter);
 
 
-app.get('/boom', (req, res, next) => {
-  throw new Error('Boom!!');
-});
 
 app.use(function (req, res, next) {
   var err = new Error('Not Found');
