03edc7ae93dd454acc0b1c8082bc6e4652831a17
RP Boyle <rpboyle11@yahoo.com>
2018-11-14 15:04:18 -0500

    added the POST request to the router

--- router/notes.router.js
+++ router/notes.router.js
@@ -63,6 +63,30 @@ router.put('/notes/:id', (req, res, next) => {
   });
 });
 
+// Post (insert) an item
+router.post('/notes', (req, res, next) => {
+  const { title, content } = req.body;
+
+  const newItem = { title, content };
+  /***** Never trust users - validate input *****/
+  if (!newItem.title) {
+    const err = new Error('Missing `title` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  notes.create(newItem, (err, item) => {
+    if (err) {
+      return next(err);
+    }
+    if (item) {
+      res.location(`http://${req.headers.host}/notes/${item.id}`).status(201).json(item);
+    } else {
+      next();
+    }
+  });
+});
+
 router.get('/boom', (req, res, next) => {
   throw new Error('Boom!!');
 });
